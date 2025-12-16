ae16079da7c5612a73867e26831af6e71686045d
omarhanigad-web <omar.hanigad@gmail.com>
2025-12-16 16:20:53 +0300

    nothing..

--- hello.py
+++ hello.py
@@ -1,13 +1,1 @@
-make=[]
-dont_make=[]
-man=input("enter the tasks you must do :::")
-for task in man:
-    user=input(f"do you finish the {task}"(yes)(no)).lower()
-    if user =="yes"
-        print("good jop")
-        make.append(task)
-    else:
-        print("you must do it today!")
-        dont_make.append(task)
-input("press Enter to exit...")
-#this a loop that i learn it last a day.
+
